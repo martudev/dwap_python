a2ba3529e7a46d63b015622df415972f4eb489a5
Martin <actsgames720@gmail.com>
2019-06-14 14:22:52 -0300

    a

--- dwap-1.0/framework/app/dwap/utils.py
+++ dwap-1.0/framework/app/dwap/utils.py
@@ -18,10 +18,8 @@ import os.path
 
 dwap_env = dict()
 
-dwap_env['origin_path'] = os.path.join(
-    os.path.dirname(os.path.realpath(sys.argv[0])))
-dwap_env['path_to_modules'] = os.path.join(os.path.dirname(
-    os.path.realpath(sys.argv[0])), 'framework', 'app', 'dwap', 'modules')
+dwap_env['origin_path'] = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])))
+dwap_env['path_to_modules'] = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'framework', 'app', 'dwap', 'modules')
 
 
 def getArgs(idx):
@@ -44,46 +42,33 @@ def printEnviroment():
         print("~  runing in default mode.             ")
         print("~                                      ")
     elif enviroment == 'help':
-        print(
-            "~  Usage: dwap <cmd> or dwap <cmd> --[options]=<value>.                ")
+        print("~  Usage: dwap <cmd> or dwap <cmd> --[options]=<value>.                ")
         print("~                                                                      ")
         print("~  Commands <cmd>:                                                     ")
         print("~ -------------------------------------------------------------------- ")
         print("~  * run      Run de WhatsApp framework as default                     ")
         print("~  * test     Run de WhatsApp framework as testing mode                ")
         print("~  * help     Show dwap help                                           ")
-        print(
-            "~  * <cmd> --[options]=<value>:   with <value>,  x_value     Example: dwap test --test_wpp=true")
+        print("~  * <cmd> --[options]=<value>:   with <value>,  x_value     Example: dwap test --test_wpp=true")
         print("~                                                y_value     For more options write 'dwap --options help'")
         print("~                                                z_value               ")
         print("~                                                                      ")
         print("~                                                                      ")
-        print(
-            "~  Options [options]:                                                  ")
+        print("~  Options [options]:                                                  ")
         print("~ -------------------------------------------------------------------- ")
         print("~  * test_wpp      Run de WhatsApp framework as default           Example: dwap test --test_wpp=true")
-        print(
-            "~                                                                      ")
-        print(
-            "~                                                                      ")
-        print(
-            "~  Example Commands:                                                   ")
-        print(
-            "~ -------------------------------------------------------------------- ")
-        print(
-            "~  * dwap run                                                          ")
-        print(
-            "~  * dwap test                                                         ")
-        print(
-            "~  * dwap help                                                         ")
-        print(
-            "~                                                                      ")
-        print(
-            "~                                                                      ")
+        print("~                                                                      ")
+        print("~                                                                      ")
+        print("~  Example Commands:                                                   ")
+        print("~ -------------------------------------------------------------------- ")
+        print("~  * dwap run                                                          ")
+        print("~  * dwap test                                                         ")
+        print("~  * dwap help                                                         ")
+        print("~                                                                      ")
+        print("~                                                                      ")
         sys.exit(0)
     else:
-        print(
-            "~  Usage: dwap <cmd> --options [options, ...].                         ")
+        print("~  Usage: dwap <cmd> --options [options, ...].                         ")
         print("~                                                                      ")
         print("~  with <cmd>,   run     Run de WhatsApp framework as default          ")
         print("~                test    Run de WhatsApp framework as testing mode     ")
@@ -93,14 +78,10 @@ def printEnviroment():
 
 
 def getChromeVersion():
-    chrome_driver = os.path.join(
-        dwap_env['path_to_modules'], 'chromedriver_74')
+    chrome_driver = os.path.join(dwap_env['path_to_modules'], 'chromedriver_74')
     chrome_version_options = Options()
-    # lo hacemos para minimizar el chrome ya que solo queremos obtener la version
-    chrome_version_options.add_argument("--headless")
-    versionDriver = webdriver.Chrome(
-        options=chrome_version_options, executable_path=chrome_driver)
-    # 'chromedriver_74' => 'version' | 'chromedriver_75' => 'browserVersion'
+    chrome_version_options.add_argument("--headless") # lo hacemos para minimizar el chrome ya que solo queremos obtener la version
+    versionDriver = webdriver.Chrome(options=chrome_version_options, executable_path=chrome_driver) # 'chromedriver_74' => 'version' | 'chromedriver_75' => 'browserVersion'
     browser_version = versionDriver.capabilities['version']
     versionDriver.close()
     return browser_version.split('.')[0]
